e854f3a0849e0afeb09dc4961bd9982be5ca4b36
adhamsalama <adhamsalama@users.noreply.github.com>
2026-06-26 08:02:46 +0000

    deploy: 513e9cf05421015897c1635f3efdac95ca896097

--- js/downloads/epubDownloader.js
+++ js/downloads/epubDownloader.js
@@ -9,7 +9,9 @@ var EpubDownloader = {
     emailSelectedArticles: function(selectedArticles, to, callback) {
         try {
             var feedTitle = getText(document.getElementById("feed-title")) || "Articles";
-            var filename = feedTitle.replace(/[^a-z0-9]/gi, "_") + "_articles.epub";
+            var _d = new Date(), _m = _d.getMonth() + 1, _day = _d.getDate();
+            var _dateStr = _d.getFullYear() + "-" + (_m < 10 ? "0" + _m : _m) + "-" + (_day < 10 ? "0" + _day : _day);
+            var filename = feedTitle.replace(/[^a-z0-9]/gi, "_") + "_articles_" + _dateStr + ".epub";
             var progressEl = document.getElementById("download-all-progress");
             removeClass(progressEl, "hidden");
             setText(progressEl, "Preparing articles for email: 0/" + selectedArticles.length);
@@ -77,7 +79,9 @@ var EpubDownloader = {
     downloadSelectedArticles: function(selectedArticles) {
         try {
             var feedTitle = getText(document.getElementById("feed-title"));
-            var filename = feedTitle.replace(/[^a-z0-9]/gi, "_") + "_selected_articles.epub";
+            var _d = new Date(), _m = _d.getMonth() + 1, _day = _d.getDate();
+            var _dateStr = _d.getFullYear() + "-" + (_m < 10 ? "0" + _m : _m) + "-" + (_day < 10 ? "0" + _day : _day);
+            var filename = feedTitle.replace(/[^a-z0-9]/gi, "_") + "_selected_articles_" + _dateStr + ".epub";
 
             if (AppConfig.USE_BACKEND) {
                 var urls = [];
